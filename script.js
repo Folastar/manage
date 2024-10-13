856bc38ab7d004aa3d7593529aa925f0a6b69d32
ogundele folarin <ogundelefolarin@gmail.com>
2024-10-13 08:45:25 +0100

    manage modified

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('#hamburger')
 const closed =document.querySelector('#close')
 const navDisplay =document.querySelector('#nav-display')
-
+const body = document.querySelector('body')
 
 
 
@@ -9,6 +9,9 @@ hamburger.addEventListener('click', ()=>{
     navDisplay.classList.remove('hidden')
     hamburger.classList.add('hidden')
     closed.classList.remove('hidden')
+    body.style.overflow='hidden'
+
+
 })
 
 
@@ -16,4 +19,6 @@ closed.addEventListener('click', ()=>{
     navDisplay.classList.add('hidden')
     hamburger.classList.remove('hidden')
     closed.classList.add('hidden')
+    body.style.overflow='hidden'
+
 })
